818a9463d7cfaafc0f16371c2094584ecc790bab
Roy-275 <anikroy275@gmail.com>
2021-09-21 12:27:07 +0600

    showing data to the page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Countries></Countries>
     </div>
   );
 }
+
+// creating my component for loading all countries here
+function Countries() {
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    fetch('https://restcountries.eu/rest/v2/all')
+      .then(res => res.json())
+      .then(data => setCountries(data))
+  }, []);
+  return (
+    <div>
+      <h1>
+        Travelling all around the world in different countries
+      </h1>
+      <h2>Countries available: {countries.length}</h2>
+      <div>
+        {
+          countries.map(country => <Country name={country.name} capital={country.capital}></Country>
+          )
+        }
+      </div>
+    </div>
+  );
+};
+
+// creating my component for showing country in webpage
+function Country(props) {
+  return (
+    <div style={{
+      background: 'orange',
+      border: '2px solid red',
+      margin: '20px',
+      borderRadius: '10px'
+    }}>
+      <h1>
+        Country name: {props.name}
+      </h1>
+      <h2>
+        Capital name: {props.capital}
+      </h2>
+    </div>
+  );
+};
 
 export default App;
